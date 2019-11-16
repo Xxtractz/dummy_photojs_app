c516694f22cfbc1a782210198e202b4450b2c505
xxtractz <musambaloyi@gmail.com>
2019-11-16 16:02:37 +0200

    camera Works

---
+++ cam.js
@@ -0,0 +1,19 @@
+/**
+ * I'm Declaring video variable so that i can access->
+ * <video id="video" autoplay></video>
+ * in my html file (index.html)
+ * document.getElemtentById("video") gets the id="video" on html
+ */
+const video = document.getElementById("video");
+
+/**
+ * 
+ */
+const _navigate = navigator.mediaDevices;
+
+ if (_navigate && _navigate.getUserMedia()) {
+     _navigate.getUserMedia({video : true}).then(function (stream){
+         video.srcObject = stream;
+     })
+ }
+
